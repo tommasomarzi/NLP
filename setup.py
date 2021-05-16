6171f155898ff7a8425fcd1066a30b5acb2c050f
Tommaso Marzi <marzitommaso@gmail.com>
2021-05-16 11:21:30 +0200

    created a separated file for requirements

--- setup.py
+++ setup.py
@@ -1,18 +1,22 @@
 import sys
 import subprocess
 import pkg_resources
+import os
 
-install_requires = {'numpy', 'pandas', 'datasets', 'transformers'}
-existing_packages = {pkg.key for pkg in pkg_resources.working_set}
-missing_packages = install_requires - existing_packages
-
-if missing_packages:
-    print("The following packages are missing and will be installed:\n{packages}".format(packages = missing_packages))
-    try:    
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', *missing_packages], stdout=subprocess.DEVNULL)
-    except subprocess.CalledProcessError:
-        print("subprocess.CalledProcessError: installation went wrong.") 
-    except OSError:
-        print("OSError: command not found.") 
-    else:
-        print("Installation was successful")
+def package_management():
+    workspace = os.path.dirname(os.path.abspath(__file__))
+    install_requires = set(line.strip() for line in open(os.path.join((sys.path[0]), 'requirements.txt')))
+    existing_packages = {pkg.key for pkg in pkg_resources.working_set}
+    missing_packages = install_requires - existing_packages
+    print(missing_packages)
+    if missing_packages:
+        print("The following packages are missing and will be installed:\n{packages}".format(packages = missing_packages))
+        try:    
+            subprocess.check_call([sys.executable, '-m', 'pip', 'install', *missing_packages], stdout=subprocess.DEVNULL)
+        except subprocess.CalledProcessError:
+            print("subprocess.CalledProcessError: installation went wrong.") 
+        except OSError:
+            print("OSError: command not found.") 
+        else:
+            print("Installation was successful.")
+package_management()
